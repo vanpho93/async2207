5428b8fa4bdb61a8d437831223437d3061dbb9f5
Van Pho Nguyen <vanpho02@gmail.com>
2017-08-12 09:58:17 +0700

    xong

--- d.js
+++ d.js
@@ -1,8 +1,5 @@
 const request = require('request');
-
-// request('http://vnexpress.net', (err, data, body) => {
-//     console.log(body);
-// });
+const fs = require('fs');
 
 function getDataFromUrl(url, cb) {
     request(url, (err, data, body) => {
@@ -10,4 +7,17 @@ function getDataFromUrl(url, cb) {
     });
 }
 
-getDataFromUrl('http://vnexpress.net', data => console.log(data));
+fs.writeFile('./x.txt', 'ksajdfja', err => console.log(err));
+
+// getDataFromUrl('http://vnexpress.net', data => console.log(data));
+
+function getDataFromUrlAndSave(url, filename, cb) {
+    request(url, (err, data, body) => {
+        if (err) return cb(err);
+        fs.writeFile(filename, body, err => {
+            cb(err);
+        });
+    });  
+}
+
+getDataFromUrlAndSave('http://dantri.com.vn', 'dantri.html', err => console.log(err));
